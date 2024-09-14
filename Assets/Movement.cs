68f65398c662d3f0248b17031fdeb2427cab1f5a
KilroyOPP <79764203+KilroyOPP@users.noreply.github.com>
2024-09-14 00:35:26 -0400

    Added Animations to player

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour
 
     float horizontal;
     float vertical;
-
+    Animator animator;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,30 @@ public class Movement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        transform.position += new Vector3(horizontal,vertical);
+        transform.position += new Vector3(horizontal, vertical);
+        HandleAnimations();
     }
+
+    private void HandleAnimations()
+    {
+        if (horizontal > 0)
+        {
+            animator.SetBool("WalkingRight", true);
+            spriteRenderer.flipX = false;
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetBool("WalkingRight", true);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.SetBool("WalkingRight", false);
+        }
+
+        animator.SetBool("WalkingUp", vertical > 0);
+        animator.SetBool("WalkingDown", vertical < 0);
+        animator.SetBool("Idle", vertical == 0 && horizontal == 0);
+    }
+       
 }
